cbbbad6ed708a2114022f2f9081ef8c8d4f7889e
Gyorgy Krajcsovits <gyorgy.krajcsovits@balabit.com>
2019-06-17 11:04:43 +0200

    Add positive test case with real recording from test environment.
    This test is using the vcrpy to record and replay HTTP request/response.
    
    Also add site_parameters.ini and pytest fixtures to handle the site
    dependent test data.

---
+++ lib/tests/test_integration.py
@@ -0,0 +1,38 @@
+#
+#   Copyright (c) 2019 One Identity
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+from ..plugin import Plugin
+from safeguard.sessions.plugin_impl.test_utils.plugin import assert_plugin_hook_result
+
+
+def test_engine_kv_v1(hc_config_engine_kv_v1, hc_account, hc_account_password):
+    plugin = Plugin(hc_config_engine_kv_v1)
+
+    result = plugin.get_password_list(
+        cookie={},
+        session_cookie={},
+        target_username=hc_account,
+    )
+
+    assert_plugin_hook_result(
+        result,
+        {'passwords': [hc_account_password]}
+    )
